5d486b8e5a648e5322d3018a97066ffe16117e8f
alex <alexpriem@gmail.com>
2016-09-01 11:56:45 +0200

    cleanup db init

--- psql/config/00-init_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
---   CREATE USER "ckan_user" WITH PASSWORD 'pass';
---   CREATE DATABASE "ckan_default" OWNER "ckan_user";
-    \connect ckan_default
-
-EOSQL
